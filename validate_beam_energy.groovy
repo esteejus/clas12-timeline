5a95172a37b5864302e2ea045afdfaa87f0c269d
Christopher Dilks <c-dilks@users.noreply.github.com>
2023-07-26 14:12:25 -0400

    refactor: use `yaml` instead

--- validate_beam_energy.groovy
+++ validate_beam_energy.groovy
@@ -1,14 +1,12 @@
-import groovy.json.JsonSlurper
+import groovy.yaml.YamlSlurper
 
-def slurper = new JsonSlurper()
-def set1 = slurper.parse(new File("config/beam-energy.json"))
+def slurper = new YamlSlurper()
+def set1 = slurper.parse(new File("config/beam-energy.yaml"))
 
-set1.each{ set ->
-
-  // loop over runs and compare to RCDB
-  println "check $set:"
-  (set['runnumMin']..set['runnumMax']).each{ runnum ->
+set1['beamEnergy'].each{ runRange ->
+  beamEn = runRange.value
+  println "Check range ${runRange.runnum} for beamEn=$beamEn"
+  (runRange.runnum.first()..runRange.runnum.last()).each{ runnum ->
     println "  $runnum:"
   }
-
 }
