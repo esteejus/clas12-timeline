e32730d67dbad08b6946856ed65b15b4025da3f8
Christopher Dilks <c-dilks@users.noreply.github.com>
2023-07-26 20:12:08 -0400

    feat: cross check with RCDB

--- validate_beam_energy.groovy
+++ validate_beam_energy.groovy
@@ -1,13 +1,43 @@
 import groovy.yaml.YamlSlurper
 import org.rcdb.*
 
+// parse beam energy config file
 def slurper = new YamlSlurper()
-def set1 = slurper.parse(new File("config/beam-energy.yaml"))
+def beamEnConfig = slurper.parse(new File("config/beam-energy.yaml"))
 
-set1['beamEnergy'].each{ runRange ->
-  beamEn = runRange.value
-  println "Check range ${runRange.runnum} for beamEn=$beamEn"
+// connect to RCDB
+def rcdbURL = System.getenv('RCDB_CONNECTION')
+if(rcdbURL==null)
+  throw new Exception("RCDB_CONNECTION not set")
+println "Connecting to $rcdbURL"
+def rcdbProvider = RCDB.createProvider(rcdbURL)
+try{ rcdbProvider.connect() }
+catch(Exception e) {
+  System.err << "ERROR: unable to connect to RCDB\n"
+  System.exit(100)
+}
+
+// loop over run ranges
+beamEnConfig['beamEnergy'].each{ runRange ->
+
+  beamEnLocal = runRange.value
+  println "Check range ${runRange.runnum}"
+
+  // loop over runs in this range
   (runRange.runnum.first()..runRange.runnum.last()).each{ runnum ->
-    println "  $runnum:"
+    print "$runnum: "
+    res = rcdbProvider.getCondition(runnum, 'beam_energy')
+    if(res==null) {
+      println " not in RCDB, skip"
+      return
+    }
+    beamEnRCDB = res.toDouble() / 1e3 // [MeV] -> [GeV]
+    print "    local=$beamEnLocal  RCDB=$beamEnRCDB"
+    if(Math.abs(beamEnLocal - beamEnRCDB)>0.01)
+      println " --> DIFFERENT"
+    else
+      println ""
   }
+  println "STOP PREMATURELY"
+  System.exit(0)
 }
