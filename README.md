2fa816aaeb3f7eb89f2611af83ed9d45d57939b5
Christopher Dilks <c-dilks@users.noreply.github.com>
2023-08-02 18:53:57 -0400

    refactor: allow `bin/slurm-mon12.sh` to run a single test job

--- README.md
+++ README.md
@@ -6,27 +6,12 @@ git clone https://github.com/JeffersonLab/clas12-timeline.git
 ```
 
 
-# How to submit `clas12_monitoring` to slurm
+# How to submit `clas12_monitoring` to `slurm`
 To submit clas12_monitoring for each run from specified directory one should run these commands, e.g.:
-<pre>
+```bash
 ./bin/build-all.sh
-<b>./bin/slurm-mon12.sh</b> version_name /volatile/clas12/rg-a/production/Spring19/mon/recon/[0-9][0-9][0-9][0-9][0-9][0-9]
-</pre>
-
-The `slurm-mon12.sh` usage:
-<pre>
-USAGE: ./bin/slurm-mon12.sh VERSION_NAME RUN_DIRECTORY[...]
-       submits the monitoring jobs to the farm batch system via Slurm. Each job corresponds to one RUN_DIRECTORY.
-
-<b>EXAMPLE: ./bin/slurm-mon12.sh version_name /volatile/clas12/rg-a/production/Spring19/mon/recon/[0-9][0-9][0-9][0-9][0-9][0-9]</b>
-
-OPTIONS:
-  VERSION_NAME         version name, defined by the user, used for slurm jobs identification
-  RUN_DIRECTORY[...]   One or more directories, each directory corresponds to one run and should contain reconstructed hipo files.
-                       The regexp can be used to specify the list of directories as well.
-                       For each RUN_DIRECTORY the directory <b>plots#RUN</b> is created in the working directory.
-                       Each `plots` directory contains hipo files with monitoring histograms.
-</pre>
+./bin/slurm-mon12.sh   # print usage guide
+```
 
 To run it interactively, see the [monitoring subdirectory](monitoring)
 
@@ -41,7 +26,7 @@ To run, execute following command,
 ```
 ./bin/detectors.sh "run group" "cooking version" "/path/to/monitoring/files/""
 ```
-with the adequte arguments, e.g.)
+with the adequate arguments, e.g.)
 ```
 ./bin/detectors.sh rgb pass0v25.18 /volatile/clas12/rg-b/offline_monitoring/pass0/v25.18/
 ```
--- bin/slurm-mon12.sh
+++ bin/slurm-mon12.sh
@@ -1,39 +1,79 @@
 #!/usr/bin/bash
 
-#test if one jar for clas12-monitoring package exists in the directory
+# usage
+if [ $# -lt 2 ]; then
+  echo """
+  USAGE: $0 [VERSION_NAME] [RUN_DIRECTORY]...
+         submits the monitoring jobs to the farm batch system via Slurm. Each job corresponds to one RUN_DIRECTORY.
+
+
+  DESCRIPTION:
+
+    [VERSION_NAME]       version name, defined by the user, used for slurm jobs identification
+
+    [RUN_DIRECTORY]...   One or more directories, each directory corresponds to
+                         one run and should contain reconstructed hipo files.
+                         - A regexp can be used to specify the list of directories as well
+                         - Globbing may also be used, if your shell supports it (e.g. * wildcard)
+                         - For each [RUN_DIRECTORY] the directory plots#RUN is created in the working
+                           directory. Each 'plots#RUN' directory contains hipo files with monitoring
+                           histograms.
+
+  EXAMPLE:
+     $0 v1.0.0 /volatile/clas12/rg-a/production/Spring19/mon/recon/*
+  """ >&2
+  exit 101
+fi
+ver=$1
+shift
+rdirs=$*
+
+# constants ############################################################
+# dependencies
 BINDIR="`dirname $0`"
 MONDIR="`realpath $BINDIR/..`"
 JARPATH="$MONDIR/monitoring/target/clas12-monitoring-v0.0-alpha.jar"
+# executable
+EXE=org.jlab.clas12.monitoring.ana_2p2
+MAX_NUM_EVENTS=100000000
+# slurm settings
+SLURM_MEMORY=1500
+SLURM_TIME=4:00:00
+########################################################################
 
+
+# test if one jar for clas12-monitoring package exists in the directory
 [[ ! -f $JARPATH ]] && echo "---- [ERROR] Problem with jar file for clas12_monitoring package --------" >&2 && echo && exit 100
 
 # test if there is a version name
-echo $1 | grep -q "/" && echo "---- [ERROR] version name must not contain / -------" >&2 && echo && exit 100
-
-ver=$1
-shift
+echo $ver | grep -q "/" && echo "---- [ERROR] version name must not contain / -------" >&2 && echo && exit 100
 echo "---- slurm job name will be: $ver"
 
-mkdir -p log plots
+# make output directories
+mkdir -p log plots slurm
 
-for rdir
-do
+# loop over input directories, building the job list
+joblist=slurm/job.${ver}.list
+> $joblist
+for rdir in $rdirs; do
 
-[[ ! -e $rdir ]] && echo "------ [ERROR] the folder $rdir does not exist" && continue
+  # check for existence of directory
+  echo "---- READ directory $rdir"
+  [[ ! -e $rdir ]] && echo "------ [ERROR] the folder $rdir does not exist" >&2 && continue
 
-run=`basename $rdir | grep -m1 -o "[0-9][0-9][0-9][0-9][0-9][0-9]"`
-[[ -z $run ]] && continue
-irun=$((10#$run))
+  # get the run number
+  runnum=`basename $rdir | grep -m1 -o -E "[0-9]+"`
+  [[ -z "$runnum" ]] && echo "------ [WARNING] unknown run number for directory $rdir" >&2 && continue
+  runnum=$((10#$runnum))
 
-[[ -e plots/plots${irun} ]] && echo "------ [WARNING] skipping run ${irun} because the directory plots/plots${irun} already exists" && continue
+  # if output directory exists, skip this run
+  plotDir=plots/plots$runnum
+  [[ -d $plotDir ]] && echo "------ [WARNING] skipping run $runnum because the directory $plotDir already exists" >&2 && continue
 
-echo "Submitting the job for run $run"
-
-mkdir plots/plots${irun}
-
-max_num_events=100000000
-
-beam_energy=`python -c "beamlist = [
+  # get the beam energy
+  # FIXME: use a config file or RCDB; this violates DRY with qa-physics/monitorRead.groovy
+  beam_energy=`python -c """
+beamlist = [
 (3861,5673,10.6), (5674, 5870, 7.546), (5871, 6000, 6.535), (6608, 6783, 10.199),
 (11620, 11657, 2.182), (11658, 12283, 10.389), (12389, 12444, 2.182), (12445, 12951, 10.389),
 (15013,15490, 5.98636), (15533,15727, 2.07052), (15728,15784, 4.02962), (15787,15884, 5.98636),
@@ -41,34 +81,65 @@ beam_energy=`python -c "beamlist = [
 
 ret=10.6
 for r0,r1,eb in beamlist:
-  if $irun>=r0 and $irun<=r1:
+  if $runnum>=r0 and $runnum<=r1:
     ret=eb
-print(ret)
-"`
+    print(ret)
+"""`
+  echo "------ beam energy = $beam_energy GeV"
 
-sbatch << END_OF_SBATCH
+  # prepare slurm script
+  echo "------ generating $EXE command"
+  cmd="""
+realpath $rdir/* > plots/$runnum.input &&
+cd plots &&
+mkdir $plotDir &&
+java -DCLAS12DIR=\${COATJAVA}/ -Xmx1024m -cp \${COATJAVA}/lib/clas/*:\${COATJAVA}/lib/utils/*:$JARPATH $EXE $runnum $runnum.input $MAX_NUM_EVENTS $beam_energy"""
+  echo $cmd >> $joblist
+
+done
+
+# check if we have any jobs to run
+if [ ! -s $joblist ]; then
+  echo "[ERROR] there are no jobs to run" >&2
+  exit 100
+fi
+
+# write slurm script
+slurm=slurm/job.${ver}.slurm
+cat > $slurm << EOF
 #!/bin/sh
 #SBATCH --ntasks=1
-#SBATCH --job-name=$ver
-#SBATCH --output=log/%x-run-$run-%j-%N.out
-#SBATCH --error=log/%x-run-$run-%j-%N.err
+#SBATCH --job-name=clas12-timeline-monitoring---$ver
+#SBATCH --output=log/%x-run-$runnum-%j-%N.out
+#SBATCH --error=log/%x-run-$runnum-%j-%N.err
 #SBATCH --partition=production
 #SBATCH --account=clas12
-#SBATCH --mem-per-cpu=1500
+
+#SBATCH --mem-per-cpu=$SLURM_MEMORY
+#SBATCH --time=$SLURM_TIME
+
+#SBATCH --array=1-$(cat $joblist | wc -l)
+#SBATCH --ntasks=1
 
 source /group/clas12/packages/setup.sh
 module load clas12/pro
 module switch clas12/pro
 
-realpath $rdir/* > plots/"$irun".input
+srun \$(head -n\$SLURM_ARRAY_TASK_ID $joblist | tail -n1)
+EOF
 
-cd plots
+echo """
+Generated:
+  Slurm script: $slurm
+  Job list:     $joblist
 
-echo "[shell] >> MAKING DIRECTORY FOR PLOTS FOR SINGLE RUN: $irun with beam energy: $beam_energy"
-mkdir plots${irun}/
+To run a single job, locally for testing, run any line in $joblist, e.g.,
+=========================
+$(head -n1 $joblist)
+=========================
 
-java -DCLAS12DIR="\${COATJAVA}/" -Xmx1024m -cp "\${COATJAVA}/lib/clas/*:\${COATJAVA}/lib/utils/*:$JARPATH" org.jlab.clas12.monitoring.ana_2p2 $irun "$irun".input $max_num_events $beam_energy
-
-END_OF_SBATCH
-
-done
+To submit all jobs to slurm, run:
+=========================
+sbatch $slurm
+=========================
+"""
